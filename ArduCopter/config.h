da562f4390046cb63db84e1346430a84bee9d0cb
Martin Schröder <mkschreder.uk@gmail.com>
2016-09-23 10:37:01 +0200

    make ptilt frame the default

--- ArduCopter/config.h
+++ ArduCopter/config.h
@@ -61,12 +61,20 @@
 //////////////////////////////////////////////////////////////////////////////
 // FRAME_CONFIG
 //
+
+// undefine frame config and force it to our frame
+#ifdef FRAME_CONFIG
+#undef FRAME_CONFIG
+#endif
+
 #ifndef FRAME_CONFIG
- # define FRAME_CONFIG   QUAD_FRAME
+ # define FRAME_CONFIG   QUAD_PTILT_FRAME
 #endif
 
 #if FRAME_CONFIG == QUAD_FRAME
  # define FRAME_CONFIG_STRING "QUAD"
+#elif FRAME_CONFIG == QUAD_PTILT_FRAME
+ # define FRAME_CONFIG_STRING "QUAD_PTILT"
 #elif FRAME_CONFIG == TRI_FRAME
  # define FRAME_CONFIG_STRING "TRI"
 #elif FRAME_CONFIG == HEXA_FRAME
